2b7f6d68d9f1b7036714072ddb6aafaa516d19eb
root <root@ip-172-31-56-198.ec2.internal>
2023-02-08 08:25:41 +0000

    terracommits

---
+++ main.tf
@@ -0,0 +1,41 @@
+provider "aws"{
+region="ap-south-1"
+access_key="AKIA6FCAHIYNBFZBP7AS"
+secret_key="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"
+}
+resource "aws_instance" "one"{
+ami="ami-01a4f99c4ac11b03c"
+instance_type="t2.medium"
+key_name = "terrakp"
+vpc_security_group_ids = [aws_security_group.three.id]
+availability_zone = "ap-south-1a"
+#!/bin/bash
+sudo -i
+yum install httpd -y
+systemctl start httpd
+chkconfig httpd on
+echo "hi this is terraform projectr created on ap -south 1 server -1" > /var/www/html/index.html
+EOF
+tags{
+Name = "swiggy-1"
+}
+}
+
+resource "aws_instance" "two"{
+ami="ami-01a4f99c4ac11b03c"
+instance_type="t2.medium"
+key_name = "terrakp"
+vpc_security_group_ids = [aws_security_group.three.id]
+availability_zone = "ap-south-1b"
+#!/bin/bash
+sudo -i
+yum install httpd -y
+systemctl start httpd
+chkconfig httpd on
+echo "hi this is terraform projectr created on ap -south 1b server -2" > /var/www/html/index.html
+EOF
+tags{
+Name = "swiggy-2"
+}
+}
+
